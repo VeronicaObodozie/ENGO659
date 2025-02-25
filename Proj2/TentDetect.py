fa87158488918bf44f67c40d9b538d491fbe299e
VeronicaObodozie <31451962+VeronicaObodozie@users.noreply.github.com>
2025-02-25 17:23:43 -0500

    Updated code and image
    Cropped image given is what TA will use.
    
    Changed threshold values and removed laplacian
    
    Added new file for noes and testing

--- Proj2/TentDetect.py
+++ Proj2/TentDetect.py
@@ -42,12 +42,12 @@ from matplotlib import pyplot as plt
 
 #----------------------Crop Region of Interest-------------------------#
 # img_pil = Image.open(r"C:\Users\Veron\OneDrive\GitHub\ENGO659\Proj2\Chad-Mille-QB-2.tif")
-img_pil = Image.open(r"/Users/wj/Downloads/ENGO 659/ENGO659/Proj2/Chad-Mille-QB-2.tif")
+# img_pil = Image.open(r"/Users/wj/Downloads/ENGO 659/ENGO659/Proj2/Chad-Mille-QB-2.tif")
 # img_pil.show()
-cropped_image = img_pil.crop((2679, 1038, 3357, 1670))
+# cropped_image = img_pil.crop((1580, 1731, 2040, 2187))
 #(left, top, right, bottom)
 # Shows the image in image viewer
-cropped_image.save('cropped_image.png')
+# cropped_image.save('cropped_image.png')
 
 #----------------------Image Enhancement-------------------------#
 # Dilate Image
@@ -61,57 +61,21 @@ strel3x3= cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 dilateMM = cv2.dilate(img, strel3x3)
 
 # Threshold
-ret, thresh = cv2.threshold(dilateMM, 127, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(dilateMM, 94, 185, cv2.THRESH_BINARY)
 
 # Filtering with Laplacian Gaussian
 gausBlur = cv2.GaussianBlur(thresh, (5, 5), 0)
-# Trying an LoG
-LoG = cv2.Laplacian(gausBlur,cv2.CV_8U)
 
 #----------------------Image Detection-------------------------#
-
 # Edge Detection: Canny: https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html? HoughLines?
-edges = cv2.Canny(LoG,100,200)
-# plt.figure(2)
-# plt.subplot(121),plt.imshow(img)
-# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(edges)
-# plt.title('Canny Edge Image'), plt.xticks([]), plt.yticks([])
-# plt.show()
+edges = cv2.Canny(gausBlur,100,200)
 
 #----------------------Counting algoritm-------------------------#
 #Count: https://www.geeksforgeeks.org/count-number-of-object-using-python-opencv/
 (cnt, hierarchy) = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.figure(3)
-cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
+plt.figure(1)
+cv2.drawContours(img, cnt, -1, (0, 255, 0), 2)
 plt.title('Detected: '+ str(len(cnt))+' Tents in Region')
-plt.imshow(rgb)
+plt.imshow(img)
 plt.show()
 print("Number of tents in region of interest: ", len(cnt))
-
-
-# # alternative code for getting the number
-# for i, c in enumerate(cnt):
-#     # 윤곽선
-#     cv2.drawContours(rgb, [c], -1, (0, 255, 0), 2)  
-
-#     # 좌표계산해 숫자넣기
-#     M = cv2.moments(c)
-#     if M["m00"] != 0:  # 면적이 0
-#         cx = int(M["m10"] / M["m00"])
-#         cy = int(M["m01"] / M["m00"])
-#     else:  # 면적이 0인 객체에 대한 임시 좌표
-#         cx, cy = c[0][0]  # 객체의 첫째 점
-    
-#     if i == 48 or i == 51:  #4 9번과 52번 인덱스 48, 51
-#         print(f"Object {i+1} - Centroid: ({cx}, {cy})")
-#     # cv2.circle(rgb, (cx, cy), 5, (0, 0, 255), -1)  # 원그리기
-#     # 번호 표시
-#     cv2.putText(rgb, str(i+1), (cx-30, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
-# plt.figure(5)
-# rgb = cv2.resize(rgb, None, fx=1.5, fy=1.5)  # 크기 1.5배 확대
-# plt.title('Detected: '+ str(len(cnt))+' Tents in Region')
-# plt.imshow(rgb)
-# plt.show()
-# print("Number of tents in region of interest: ", len(cnt))
